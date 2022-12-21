ddc6dfcdc9bb1d1b5f399403a2ac194e4d6a8ff7
Alpay96 <alpayustalar@hotmail.com>
2022-12-21 13:54:58 +0300

    Add project files.

---
+++ JobTrackingForm/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobTrackingForm
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        BimserEntities db;
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            db = new BimserEntities();
+            db.Jobs.Load();
+            jobBindingSource.DataSource = db.Jobs.Local;
+        }
+
+        private void btnAdd_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            jobBindingSource.AddNew();
+        }
+
+        private void btnDelete_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (XtraMessageBox.Show("Bu kaydı silmek istediğinizden emin misiniz?", "Mesaj", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                jobBindingSource.RemoveCurrent();
+        }
+
+        private void btnUpdate_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            db.SaveChanges();
+            XtraMessageBox.Show("Verileriniz başarı ile kaydedildi.", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void barButtonItem4_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            var changed = db.ChangeTracker.Entries().Where(x => x.State != EntityState.Unchanged).ToList();
+            foreach (var obj in changed)
+            {
+                switch (obj.State)
+                {
+                    case EntityState.Modified:
+                        obj.CurrentValues.SetValues(obj.OriginalValues);
+                        obj.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        obj.State = EntityState.Detached;
+                        break;
+                    case EntityState.Deleted:
+                        obj.State = EntityState.Unchanged;
+                        break;
+                }
+            }
+            jobBindingSource.ResetBindings(false);
+        }
+
+        private void gridControl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
